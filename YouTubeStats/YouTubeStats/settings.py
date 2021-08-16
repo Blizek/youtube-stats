8407c0cd9e9cc0b768d3c38b7cc27917c4b63c37
Blizek <blazej.naziemiec@gmail.com>
2021-08-16 14:59:07 +0200

    added static root

--- YouTubeStats/YouTubeStats/settings.py
+++ YouTubeStats/YouTubeStats/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-%ej9g#ktf2yhdm=#cq%dll$767_r7e2!l(6sc4pn^)18_+04#=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['youtube-stats-app.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['gentle-harbor-52878.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
@@ -119,6 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
